084b681510f6bed67aef3611b13cffc225fd3522
yamachu <yamachu.dev@gmail.com>
2022-09-20 02:21:17 +0900

    とりあえず動く

--- src/Records.cs
+++ src/Records.cs
@@ -2,7 +2,8 @@ using System.Collections.Generic;
 
 namespace NetWebAssemblyTSTypeGenerator
 {
-    internal sealed record Binds(IEnumerable<MethodDefinition> Imports, IEnumerable<MethodDefinition> Exports);
-    internal sealed record MethodDefinition(string FullName, IEnumerable<Argument> Arguments /* TODO: ReturnType */);
+    internal sealed record Binds(IEnumerable<ImportMethodDefinition> Imports, IEnumerable<ExportMethodDefinition> Exports);
+    internal sealed record ExportMethodDefinition(string FullName, IEnumerable<Argument> Arguments /* TODO: ReturnType */);
+    internal sealed record ImportMethodDefinition(string ModuleName, string FunctionName, IEnumerable<Argument> Arguments /* TODO: ReturnType */);
     internal sealed record Argument(string Name /* TODO: Type */);
 }
